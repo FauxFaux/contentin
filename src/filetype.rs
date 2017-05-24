5b1bbfd0e9648435a735b7d4b2dfa6bdcc9658f4
Chris West (Faux) <git@goeswhere.com>
2017-05-24 22:03:54 +0100

    move filetype to a new file

---
+++ src/filetype.rs
@@ -0,0 +1,103 @@
+use std;
+use std::fmt;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum FileType {
+    GZip,
+    Zip,
+    Tar,
+    BZip2,
+    Xz,
+    Deb,
+    Other,
+}
+
+impl fmt::Display for FileType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+fn read_octal(bytes: &[u8]) -> Option<u32> {
+    let mut start = 0;
+    while start < bytes.len() && b' ' == bytes[start] {
+        start += 1;
+    }
+
+    let mut end = bytes.len() - 1;
+    while end > start && (b' ' == bytes[end] || 0 == bytes[end]) {
+        end -= 1;
+    }
+
+    if let Ok(string) = std::str::from_utf8(&bytes[start..(end+1)]) {
+        if let Ok(val) = u32::from_str_radix(string, 8) {
+            return Some(val);
+        }
+    }
+    None
+}
+
+fn is_probably_tar(header: &[u8]) -> bool {
+    if header.len() < 512 {
+        return false;
+    }
+
+    if let Some(checksum) = read_octal(&header[148..156]) {
+        let mut sum: u32 = (b' ' as u32) * 8;
+        for i in 0..148 {
+            sum += header[i] as u32;
+        }
+        for i in 156..512 {
+            sum += header[i] as u32;
+        }
+
+        if checksum == sum {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+const DEB_PREFIX: &[u8] = b"!<arch>\ndebian-binary ";
+
+impl FileType {
+    pub fn identify<'a>(header: &[u8]) -> FileType {
+        if header.len() >= 20
+            && 0x1f == header[0] && 0x8b == header[1] {
+            FileType::GZip
+        } else if header.len() >= 152
+            && b'P' == header[0] && b'K' == header[1]
+            && 0x03 == header[2] && 0x04 == header[3] {
+            FileType::Zip
+        } else if header.len() > 257 + 10
+            && b'u' == header[257] && b's' == header[258]
+            && b't' == header[259] && b'a' == header[260]
+            && b'r' == header[261]
+            && (
+            (0 == header[262] && b'0' == header[263] && b'0' == header[264]) ||
+                (b' ' == header[262] && b' ' == header[263] && 0 == header[264])
+        ) {
+            FileType::Tar
+        } else if header.len() > 70
+            && header[0..DEB_PREFIX.len()] == DEB_PREFIX[..]
+            && header[66..70] == b"`\n2."[..] {
+            FileType::Deb
+        } else if header.len() > 40
+            && b'B' == header[0] && b'Z' == header[1]
+            && b'h' == header[2] && 0x31 == header[3]
+            && 0x41 == header[4] && 0x59 == header[5]
+            && 0x26 == header[6] {
+            FileType::BZip2
+        } else if header.len() > 6
+            && 0xfd == header[0] && b'7' == header[1]
+            && b'z' == header[2] && b'X' == header[3]
+            && b'Z' == header[4] && 0 == header[5] {
+            FileType::Xz
+        } else if is_probably_tar(header) {
+            FileType::Tar
+        } else {
+            FileType::Other
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,10 @@ mod tee;
 use tee::*;
 
 mod stat;
+use stat::Stat;
+
+mod filetype;
+use filetype::FileType;
 
 // magic:
 use std::io::Write;
@@ -141,7 +145,7 @@ impl<'a> Unpacker<'a> {
 
     fn from_file<'b>(path: &str, fd: &fs::File, options: &'b Options) -> io::Result<Unpacker<'b>> {
         let meta = fd.metadata()?;
-        let stat = stat::Stat::from(&meta);
+        let stat = Stat::from(&meta);
         Ok(Unpacker {
             options,
             current: FileDetails {
@@ -191,42 +195,6 @@ impl<'a> Unpacker<'a> {
     }
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-enum FileType {
-    GZip,
-    Zip,
-    Tar,
-    BZip2,
-    Xz,
-    Deb,
-    Other,
-}
-
-impl fmt::Display for FileType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        fmt::Debug::fmt(self, f)
-    }
-}
-
-fn read_octal(bytes: &[u8]) -> Option<u32> {
-    let mut start = 0;
-    while start < bytes.len() && b' ' == bytes[start] {
-        start += 1;
-    }
-
-    let mut end = bytes.len() - 1;
-    while end > start && (b' ' == bytes[end] || 0 == bytes[end]) {
-        end -= 1;
-    }
-
-    if let Ok(string) = std::str::from_utf8(&bytes[start..(end+1)]) {
-        if let Ok(val) = u32::from_str_radix(string, 8) {
-            return Some(val);
-        }
-    }
-    None
-}
-
 fn simple_time(dur: time::Duration) -> u64 {
     dur.as_secs().checked_mul(1_000_000_000)
         .map_or(0, |nanos| nanos + dur.subsec_nanos() as u64)
@@ -263,68 +231,7 @@ fn simple_time_ctime(val: &stat::Stat) -> u64 {
     }
 }
 
-fn is_probably_tar(header: &[u8]) -> bool {
-    if header.len() < 512 {
-        return false;
-    }
 
-    if let Some(checksum) = read_octal(&header[148..156]) {
-        let mut sum: u32 = (b' ' as u32) * 8;
-        for i in 0..148 {
-            sum += header[i] as u32;
-        }
-        for i in 156..512 {
-            sum += header[i] as u32;
-        }
-
-        if checksum == sum {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-const DEB_PREFIX: &[u8] = b"!<arch>\ndebian-binary ";
-
-fn identify<'a>(header: &[u8]) -> FileType {
-    if header.len() >= 20
-        && 0x1f == header[0] && 0x8b == header[1] {
-        FileType::GZip
-   } else if header.len() >= 152
-        && b'P' == header[0] && b'K' == header[1]
-        && 0x03 == header[2] && 0x04 == header[3] {
-        FileType::Zip
-    } else if header.len() > 257 + 10
-        && b'u' == header[257] && b's' == header[258]
-        && b't' == header[259] && b'a' == header[260]
-        && b'r' == header[261]
-        && (
-            (0 == header[262] && b'0' == header[263] && b'0' == header[264]) ||
-            (b' ' == header[262] && b' ' == header[263] && 0 == header[264])
-        ) {
-        FileType::Tar
-    } else if header.len() > 70
-        && header[0..DEB_PREFIX.len()] == DEB_PREFIX[..]
-        && header[66..70] == b"`\n2."[..] {
-        FileType::Deb
-    } else if header.len() > 40
-        && b'B' == header[0] && b'Z' == header[1]
-        && b'h' == header[2] && 0x31 == header[3]
-        && 0x41 == header[4] && 0x59 == header[5]
-        && 0x26 == header[6] {
-        FileType::BZip2
-    } else if header.len() > 6
-        && 0xfd == header[0] && b'7' == header[1]
-        && b'z' == header[2] && b'X' == header[3]
-        && b'Z' == header[4] && 0 == header[5] {
-        FileType::Xz
-    } else if is_probably_tar(header) {
-        FileType::Tar
-    } else {
-        FileType::Other
-    }
-}
 
 #[derive(Clone, Copy, Debug)]
 struct Rewind {
@@ -411,8 +318,7 @@ impl<'a> Unpacker<'a> {
             return Err(io::Error::new(io::ErrorKind::Other, Rewind {}));
         }
 
-
-        let identity = identify(fd.fill_buf()?);
+        let identity = FileType::identify(fd.fill_buf()?);
         self.log(2, || format!("identified as {}", identity))?;
         match identity {
             FileType::GZip => {
